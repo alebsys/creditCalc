4f580470da59b0c274e33cd834f9c9476b691278
Aleksandr Lebedev <lebedevam88@gmail.com>
2020-09-06 12:59:08 +0300

    v0.0.1

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"os"
+	"text/tabwriter"
 )
 
 var creditSum, creditTerm, creditPercent float64
@@ -11,21 +13,22 @@ func main() {
 
 	creditSum, creditTerm, creditPercent = question()
 	payMonth := annuityPay(creditSum, creditTerm, creditPercent)
-	overPay := totalAmount(payMonth, creditTerm)
-	answer(payMonth, overPay)
+	overPay := totalAmount(creditSum, payMonth, creditTerm)
+	answer(payMonth, overPay, creditSum, creditTerm, creditPercent)
+
 }
 
 // Функция получает исходные данные у пользователя
 func question() (creditSum, creditTerm, creditPercent float64) {
-	fmt.Println("Введите сумму кредита, в рублях")
+	fmt.Println("Введите сумму кредита, в рублях:")
 	fmt.Scanf("%f", &creditSum)
 	fmt.Println("\n")
 
-	fmt.Println("Введите срок кредита, в месяцах")
+	fmt.Println("Введите срок кредита, в месяцах:")
 	fmt.Scanf("%f", &creditTerm)
 	fmt.Println("\n")
 
-	fmt.Println("Введите процент по кредиту, простым числом")
+	fmt.Println("Введите процент по кредиту, простым числом:")
 	fmt.Scanf("%f", &creditPercent)
 	fmt.Println("\n")
 
@@ -40,13 +43,29 @@ func annuityPay(cs, ct, cp float64) float64 {
 	return cs * (percentMonth + percentMonth/(x-1))
 }
 
-// Функция считает общую сумму платежей
-func totalAmount(pm, ct float64) float64 {
-	return pm * ct
+// Функция высчитывает сумму переплаты
+func totalAmount(cs, pm, ct float64) float64 {
+	return pm*ct - cs
 }
 
 // Функция выводит на экран результат
-func answer(pm, op float64) {
-	fmt.Printf("Ваш эжемесячный платеж равен: %.2f\n", pm)
-	fmt.Printf("Общая сумма платежей: %.2f\n", op)
+func answer(pm, op, cs, ct, cp float64) {
+
+	w := new(tabwriter.Writer)
+
+	// minwidth, tabwidth, padding, padchar, flags
+	w.Init(os.Stdout, 8, 8, 0, '\t', 0)
+
+	defer w.Flush()
+
+	fmt.Fprintf(w, "\n %s\t%s\t%s\t%s\t%s", "Месяцы", "Ежемесячный платеж", "Погашение процентов", "Погашение тела кредита", "Долг на конец месяца")
+	fmt.Fprintf(w, "\n %s\t%s\t%s\t%s\t%s", "--------", "--------", "--------", "--------", "--------")
+
+	for i := 1; i <= int(ct); i++ {
+		percAnnuity := creditSum * creditPercent / 100 / 12
+		payTelo := pm - percAnnuity
+		creditSum = creditSum - payTelo
+
+		fmt.Fprintf(w, "\n %d\t%.0f\t%.0f\t%.0f\t%.0f\t", i, pm, percAnnuity, payTelo, creditSum)
+	}
 }

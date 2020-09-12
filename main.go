d92d89680086173118f435b7ef587446e4084604
Aleksandr Lebedev <lebedevam88@gmail.com>
2020-09-12 11:11:13 +0300

    Update internal logic & add func "Общая сумма переплаты"

--- main.go
+++ main.go
@@ -5,83 +5,57 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"strings"
 	"text/tabwriter"
 )
 
-var creditSum, creditTerm, creditPercent, dosro4kaSum float64
+var dosro4kaSum, payMonth float64
 var dosro4kaMonth int
 var answerDosr string
 var dosro4ka = make(map[int]float64)
 
 func main() {
 
-	creditSumPtr := flag.Float64("sum", 0, "Credit sum")
-	creditTermPtr := flag.Float64("period", 0, "Credit period in month")
-	creditPercentPtr := flag.Float64("percent", 0, "Annual credit interest")
-
+	creditSumFlag := flag.Float64("s", 0, "Сумма кредита, в рублях")
+	creditTimeFlag := flag.Float64("t", 0, "Срок кредита, в месяцах")
+	creditPercentFlag := flag.Float64("p", 0, "Годовой кредитный процент")
 	flag.Parse()
 
-	fmt.Println("Будет ли досрочка?(да/нет)")
-	fmt.Scanf("%v", &answerDosr)
+	dosro4kaInfo()
 
-	if answerDosr == "да" {
+	answer(*creditSumFlag, *creditTimeFlag, *creditPercentFlag)
+}
 
-		for answerDosr != "нет" {
+// Функция собирает данные о досрочных платежах
+func dosro4kaInfo() map[int]float64 {
 
-			fmt.Println("Введите число месяца")
+	fmt.Print("Будут ли досрочные платежи?(да/нет): ")
+	fmt.Scanf("%s", &answerDosr)
+
+	if strings.ToLower(answerDosr) == "да" {
+
+		for strings.ToLower(answerDosr) != "нет" {
+
+			fmt.Print("Введите число месяца: ")
 			fmt.Scanf("%d", &dosro4kaMonth)
-			fmt.Println("и сумму досрочного погашения")
+			fmt.Print("Введите сумму досрочного погашения: ")
 			fmt.Scanf("%f", &dosro4kaSum)
 
 			dosro4ka[dosro4kaMonth] = dosro4kaSum
 
-			fmt.Println("Будут ли еще досрочные платежи?(да/нет)")
+			fmt.Print("Будут ли еще досрочные платежи?(да/нет): ")
 
 			fmt.Scanf("%v", &answerDosr)
 
 		}
 	}
-
-	answer(*creditSumPtr, *creditTermPtr, *creditPercentPtr)
-
+	return dosro4ka
 }
 
-// Функция получает исходные данные у пользователя
-func question() (creditSum, creditTerm, creditPercent float64) {
-	fmt.Println("Введите сумму кредита, в рублях:")
-	fmt.Scanf("%f", &creditSum)
-	fmt.Println("\n")
-
-	fmt.Println("Введите срок кредита, в месяцах:")
-	fmt.Scanf("%f", &creditTerm)
-	fmt.Println("\n")
-
-	fmt.Println("Введите процент по кредиту, простым числом:")
-	fmt.Scanf("%f", &creditPercent)
-	fmt.Println("\n")
-
-	fmt.Println("-----------------")
-	return creditSum, creditTerm, creditPercent
-}
-
-// Функция считает аннуитентный платеж
-func annuityPay(cs, ct, cp float64) float64 {
-	percentMonth := cp / 100 / 12
-	x := (math.Pow((1 + percentMonth), ct))
-	return cs * (percentMonth + percentMonth/(x-1))
-}
-
-// Функция высчитывает сумму переплаты
-func totalAmount(cs, pm, ct float64) float64 {
-	return pm*ct - cs
-}
-
-// Функция выводит на экран результат
+// Функция выводит на экран результат расчетов
 func answer(cs, ct, cp float64) {
 
 	w := new(tabwriter.Writer)
-
-	// minwidth, tabwidth, padding, padchar, flags
 	w.Init(os.Stdout, 8, 8, 0, '\t', 0)
 
 	defer w.Flush()
@@ -89,13 +63,16 @@ func answer(cs, ct, cp float64) {
 	fmt.Fprintf(w, "\n %s\t%s\t%s\t%s\t%s", "Месяцы", "Ежемесячный платеж", "Погашение процентов", "Погашение тела кредита", "Долг на конец месяца")
 	fmt.Fprintf(w, "\n %s\t%s\t%s\t%s\t%s", "--------", "--------", "--------", "--------", "--------")
 
-	// dosro4ka[4] = 10000
-
 	payMonth := annuityPay(cs, ct, cp)
 
-	for i := 1; cs >= 0; i++ {
+	s := "Итоговые расчеты:"
+	d := 110 // shell width
 
-		// overPay := totalAmount(creditSum, payMonth, creditTerm)
+	fmt.Printf(fmt.Sprintf("%%-%ds", d/2), fmt.Sprintf(fmt.Sprintf("%%%ds\n\n", d/2), s))
+
+	fmt.Printf("Общая сумма переплаты: %.2f\n\n", totalAmount(cs, payMonth, ct))
+
+	for i := 1; cs >= 0; i++ {
 
 		percAnnuity := cs * cp / 100 / 12
 		payTelo := payMonth - percAnnuity
@@ -109,4 +86,17 @@ func answer(cs, ct, cp float64) {
 
 		fmt.Fprintf(w, "\n %d\t%.2f\t%.2f\t%.2f\t%.2f\t", i, payMonth, percAnnuity, payTelo, cs)
 	}
+
+}
+
+// Функция считает аннуитентный платеж
+func annuityPay(cs, ct, cp float64) float64 {
+	percentMonth := cp / 100 / 12
+	x := (math.Pow((1 + percentMonth), ct))
+	return cs * (percentMonth + percentMonth/(x-1))
+}
+
+// Функция считает сумму переплаты
+func totalAmount(cs, pm, ct float64) float64 {
+	return pm*ct - cs
 }
